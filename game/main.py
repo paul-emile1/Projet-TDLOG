8f98f095f0f6af66b92100e755349360b43aec4f
Paul-Emile Marcus <paulem.marcus@gmail.com>
2025-12-05 14:46:50 +0100

    Delete game/main.py

--- game/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if __name__ == "__main__":
-    try:
-        from controller import Controller
-        app_ctrl = Controller()
-        app_ctrl.start()
-    except KeyboardInterrupt:
-        sys.exit(0)
